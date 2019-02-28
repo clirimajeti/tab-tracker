77433d336ea4b106e45da9857f9cb4645c9d31d4
unknown <clirimajeti@gmail.com>
2019-02-28 14:41:34 +0100

    login

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -3,13 +3,41 @@ const {User} = require('../models')
 module.exports = {
     async register (req,res) {
         try{
-            const user = await User.create(req.body);
+            const user = await User.create(req.body)
             res.send(user.toJSON())
         } catch (err) {
             res.status('400').send({
                 error: 'This email account is already in use.'
             })
+        }  
+    },
+    async login () {
+        try{
+            const {email, password} = req.body;
+            const user = await User.findOne({
+                where: {
+                    email: email
+                }
+            })
+            if(!user){
+               return res.status(403).send({
+                    error:"Login information is wrong"
+                })
+            }
+            const isPasswordValid = password === user.password;
+            if(!isPasswordValid){
+                return res.status(403).send({
+                    error:"Login information is wrong"
+                })
+            }
+            const userJson = user.toJSON()
+            res.send({
+                user: userJson
+            })
+        } catch (err) {
+            res.status('400').send({
+                error: 'This email account is already in use.'
+            })
         }
-        
     }
 }
