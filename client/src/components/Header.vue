396bca6a4bccf3a9be1b8762b3de23e769c0bdbd
unknown <clirimajeti@gmail.com>
2019-02-28 12:54:39 +0100

    login

---
+++ client/src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-toolbar fixed dark class="cyan">
+    <v-toolbar-items>
+      <v-btn flat dark to="/" class="mr-4">
+        Tab Tracker
+      </v-btn>
+    </v-toolbar-items>
+   <!-- <v-toolbar-items>
+      <v-btn flat dark>Browser</v-btn>
+    </v-toolbar-items> -->
+    <v-spacer></v-spacer>
+    <v-toolbar-items>
+      <v-btn flat dark router to="login">
+        Login
+      </v-btn>
+      <v-btn flat dark router to="register">
+        Sign Up
+      </v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+</style>
--- server/src/routes.js
+++ server/src/routes.js
@@ -5,7 +5,10 @@ module.exports = (app) => {
     app.post('/register',
       AuthenticationControllerPolicy.register,
       AuthenticationController.register
-      )
+      ),
+    app.post('/login',
+      AuthenticationController.login
+      )  
     }
 
 
